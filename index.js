56da9a9fbb767edd875f30ce210f71adee3c9051
S. Marie Kav <siobhan.harley@gmail.com>
2017-01-08 23:38:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 function dwarfRollCall(dwarves) {
-}
+  var withNumbers = [];
+  for(var x = 0; x < dwarves.length; x++){
+    withNumbers.push((x+1) + '. ' + dwarves[x]);
+    var dwarfList = withNumbers.join(' ');
+  }
+  return dwarfList + ' ';
+  }
 
 function summonCaptainPlanet(planeteerCalls){
+  var newArr = [];
+  for(var x = 0; x < planeteerCalls.length; x++) {
+    newArr.push(planeteerCalls[x].toUpperCase() + '!')
+  }
+  return newArr;
 }
 
 function longPlaneteerCalls(words) {
+  for(var x = 0; x < words.length; x++) {
+    if(words[x].length >= 5) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 }
 
 function findTheCheese (foods) {
+  for(var x = 0; x < foods.length; x++) {
+    if (foods[x] === 'cheddar') {
+    var result = foods[x];
+    break;
+    }
+    else {
+      var result = 'no cheese!';
+    }
+  }
+  return result;
 }
